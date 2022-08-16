eea2bf5e0430e7d68842139f4522668f171bf06f
jcrisostomo1 <joshuacr22@gmail.com>
2022-08-15 23:05:10 -0400

    add scoreboard

--- src/App.js
+++ src/App.js
@@ -2,12 +2,18 @@ import './styles/App.css';
 import 'font-awesome/css/font-awesome.min.css';
 
 function App() {
+  let highScore = 0;
+  let score = 0;
   return (
     <div>
       <header>
         <h1>Odin Memory Game</h1>
+        <div className='scoreboard'>
+          <h3>Score: {score}</h3>
+          <h3>High Score: {highScore}</h3>
+        </div>
       </header>
-      <main></main>
+      
       <footer>
         <a href="https://github.com/jcrisostomo1" rel="noreferrer" target="_blank"><i className="fa fa-github fa-lg"></i></a>
         <p>Made by: jcrisostomo1</p>
